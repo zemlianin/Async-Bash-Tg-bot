5f7dfa8c86362f7e3382a09a4b551eb77fdb6a7b
zemlianin <detulie@mail.ru>
2024-07-01 18:24:37 +0300

    add killer

--- main.sh
+++ main.sh
@@ -4,13 +4,15 @@ source extensions/using.sh
 using extensions
 
 test_job(){
-   echo 'penis'
+   echo "job func pid:$$"
 }
 
 main() {
    build src
    add_job test_job 2 5
    job_runner_start
+   process_killer_start
+   sleep 10
 }
 
 main
